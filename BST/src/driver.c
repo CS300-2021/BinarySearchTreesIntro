54886ca523abeb44a18fed5c66ee27851f80a8f5
ShereenKhoja <ShereenKhoja@gmail.com>
2021-10-31 22:53:29 -0700

    Implemented and tested all functions

--- BST/src/driver.c
+++ BST/src/driver.c
@@ -1,3 +1,13 @@
+/**************************************************************************
+ File name:		BSTdriver.c
+ Author:        CS, Pacific University
+ Date:          10.29.2021
+ Class:         CS300
+ Assignment:    Binary Search Trees
+ Purpose:       This file defines the data structure for a Binary Search
+                Tree
+ *************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,8 +22,22 @@ typedef struct BSTNode {
 	BSTNodePtr psLeftChild, psRightChild;
 } BSTNode;
 
+/**************************************************************************
+ Function: 	 	bstInsert
+
+ Description:   Inserts a node into a Binary Search Tree
+
+ Parameters:	hsRoot - handle to the root of the BST
+                key    - the key to be placed in the node
+                         used to organize the BST
+                szData - the data to be placed in the node
+                         can be copied into the node using strncpy()
+                size   - number of characters in szData
+
+ Returned:	 	None
+ *************************************************************************/
 void bstInsert(BSTNode **hsRoot, int key, char *szData, int size) {
-	BSTNode *psTmp = *hsRoot, *psParent = *hsRoot;
+	BSTNodePtr psTmp = *hsRoot, psParent = *hsRoot;
 	if ( NULL == *hsRoot) {
 		*hsRoot = (BSTNodePtr) malloc(sizeof(BSTNode));
 		(*hsRoot)->psLeftChild = (*hsRoot)->psRightChild = NULL;
@@ -45,18 +69,40 @@ void bstInsert(BSTNode **hsRoot, int key, char *szData, int size) {
 	}
 }
 
-void BSTPrintInorder(BSTNodePtr psNode) {
+/**************************************************************************
+ Function: 	 	bstPrintInorder
+
+ Description:   Prints the Binary Search Tree in order
+
+ Parameters:	psNode - pointer to the node
+
+ Returned:	 	None
+ *************************************************************************/
+void bstPrintInorder(BSTNodePtr psNode) {
 	if (psNode == NULL) {
 		return;
 	}
-	BSTPrintInorder(psNode->psLeftChild);
+	bstPrintInorder(psNode->psLeftChild);
 	printf("(%d,%s)-", psNode->key, psNode->szData);
-	BSTPrintInorder(psNode->psRightChild);
+	bstPrintInorder(psNode->psRightChild);
 }
 
-bool bstSearch(BSTNode *psRoot, int key, char *szData, int size) {
+/**************************************************************************
+ Function: 	 	bstSearch
+
+ Description:   Searches for a node in the BST
+
+ Parameters:	psRoot - pointer to the root of the BST
+                key    - the key we are searching for
+                szData - will contain the data from the node
+                         can be copied using strncpy()
+                size   - number of characters in szData
+
+ Returned:	 	Result of if the node is found in the BST
+ *************************************************************************/
+bool bstSearch(BSTNodePtr psRoot, int key, char *szData, int size) {
 	bool bFound = false;
-	BSTNode *psTmp = psRoot;
+	BSTNodePtr psTmp = psRoot;
 	while (NULL != psTmp && psTmp->key != key) {
 		if (key < psTmp->key) {
 			psTmp = psTmp->psLeftChild;
@@ -71,10 +117,21 @@ bool bstSearch(BSTNode *psRoot, int key, char *szData, int size) {
 	return bFound;
 }
 
-bool bstFindLevel(BSTNode *psRoot, int key, int *pLevel) {
+/**************************************************************************
+ Function: 	 	bstFindLevel
+
+ Description:   Finds the level of a node
+
+ Parameters:	psRoot - pointer to the root of the BST
+                key    - the key to be searched for in the BST
+                pLevel - the level of the node
+
+ Returned:	 	Result of if the node is found in the BST
+ *************************************************************************/
+bool bstFindLevel(BSTNodePtr psRoot, int key, int *pLevel) {
 	bool bFound = false;
 	int level = 0;
-	BSTNode *psTmp = psRoot;
+	BSTNodePtr psTmp = psRoot;
 	while ( NULL != psTmp && psTmp->key != key) {
 		if (key < psTmp->key) {
 			psTmp = psTmp->psLeftChild;
@@ -90,15 +147,47 @@ bool bstFindLevel(BSTNode *psRoot, int key, int *pLevel) {
 	return bFound;
 }
 
-int main() {
-	BSTNodePtr psTree;
-	bstInsert(&psTree, 15, "Shereen", 8);
-	bstInsert(&psTree, 8, "Layla", 6);
-	bstInsert(&psTree, 20, "Nora", 5);
-	bstInsert(&psTree, 200, "Mum", 4);
-	bstInsert(&psTree, 10, "Dad", 4);
+/**************************************************************************
+ Function: 	 	main()
 
-	BSTPrintInorder(psTree);
-	printf("YAY!");
+ Description:   Used to create and test the BST
+
+ Parameters:	none
+
+ Returned:	 	none
+ *************************************************************************/
+int main() {
+	BSTNodePtr psTree = NULL;
+	char szFound[MAX] = "x";
+	int key, level;
+
+	bstPrintInorder(psTree);
+
+	bstInsert(&psTree, 15, "Bowser", 8);
+	bstInsert(&psTree, 8, "Mario", 6);
+	bstInsert(&psTree, 20, "Luigi", 5);
+	bstInsert(&psTree, 200, "Toad", 4);
+	bstInsert(&psTree, 10, "Yoshi", 4);
+
+	bstPrintInorder(psTree);
+
+	key = 50;
+
+	if(bstSearch(psTree, key, szFound, MAX)) {
+		printf("\n\nFound %d: %s", key, szFound);
+	} else {
+		printf("\n\nDid not find %d", key);
+	}
+
+	key = 15;
+
+		if(bstFindLevel(psTree, key, &level)) {
+			printf("\n\nFound %d at level %d", key, level);
+		} else {
+			printf("\n\nDid not find %d", key);
+		}
+
+	printf("\n\nReached the end!");
+
 	return EXIT_SUCCESS;
 }
